127699109bc5696cdcfc7cc6839a45eda7981aea
Tejas Reddy <tejasreddy353@gmail.com>
2026-03-15 00:33:39 +0530

    Player screen UI

--- src/app/(protected)/player.tsx
+++ src/app/(protected)/player.tsx
@@ -1,14 +1,39 @@
-import { Entypo } from "@expo/vector-icons";
+import { Entypo, Ionicons } from "@expo/vector-icons";
 import { router } from "expo-router";
-import { Pressable, Text, View } from "react-native";
+import { Image, Pressable, Text, View } from "react-native";
+import books from "../../dummyBooks";
+import { SafeAreaView } from "react-native-safe-area-context";
+import PlayBackBar from "../../components/PlayBackBar";
 
 export default function Player() {
+  const book = books[0];
   return (
-    <View className="flex-1 bg-gray-900 items-center justify-center">
-      <Pressable onPress={() => router.back()} className="absolute top-16 left-4 bg-gray-800 rounded-full p-2">
+    <SafeAreaView className="flex-1 bg-gray-900 p-4 py-10 gap-4">
+      <Pressable
+        onPress={() => router.back()}
+        className="absolute top-16 left-4 bg-gray-800 rounded-full p-2"
+      >
         <Entypo name="chevron-down" size={24} color="white" />
       </Pressable>
-      <Text className="text-white">Player</Text>
-    </View>
+      <Image
+        source={{ uri: book.thumbnail_url }}
+        className="w-[95%] aspect-square rounded-[30px] self-center"
+      />
+
+      <View className="gap-8 flex-1 justify-end">
+        <Text className="text-white text-2xl font-bold text-center">
+          {book.title}
+        </Text>
+        <PlayBackBar value={0.5} />
+
+        <View className="flex-row items-center justify-between">
+          <Ionicons name="play-skip-back" size={24} color="white" />
+          <Ionicons name="play-back" size={24} color="white" />
+          <Ionicons name="play" size={50} color="white" />
+          <Ionicons name="play-forward" size={24} color="white" />
+          <Ionicons name="play-skip-forward" size={24} color="white" />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
