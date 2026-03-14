fb75570f11aad5fa73754b7c016bd740dc41eacc
Tejas Reddy <tejasreddy353@gmail.com>
2026-03-14 16:45:00 +0530

    Updated to FlatList from map

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { View, FlatList } from 'react-native';
 import './global.css'
 import books from './src/dummyBooks'
 import BookListItem from './src/components/BookListItem';
 
 export default function App() {
   return (
-    <View className='bg-slate-800 flex-1 p-4 justify-center'>
-      {books.map((book, i) => (
-        <BookListItem key={i} book={book}/>
-      ))}
+    <View className='bg-slate-800 flex-1 p-4 justify-center pt-20'>
+
+      <FlatList 
+        data={books} 
+        contentContainerClassName='gap-4'
+        renderItem={({item}) => <BookListItem book={item} />} 
+      />
       <StatusBar style="auto" />
     </View>
   );
--- src/components/BookListItem.tsx
+++ src/components/BookListItem.tsx
@@ -4,7 +4,7 @@ import { AntDesign } from '@expo/vector-icons';
 
 export default function BookListItem({book}: {book: Book}) {
   return (
-       <View className='flex-row gap-4 items-center mb-4'>
+       <View className='flex-row gap-4 items-center'>
           <Image source={{uri: book.thumbnail_url}} className='w-16 aspect-square rounded-lg' />
           <View className='gap-1 flex-1'>
             <Text className='text-2xl text-gray-100 font-bold'>{book.title}</Text>
