3dc3265d06b3b31bd7adcb17f119542c5123cf11
Tejas Reddy <tejasreddy353@gmail.com>
2026-03-14 15:10:08 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import './global.css'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className='bg-slate-950 flex-1 items-center justify-center'>
+      <Text className='text-2xl text-gray-100 font-bold'>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
