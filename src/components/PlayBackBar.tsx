a645aa0109dd98dcbf4a775622adca859d432255
Tejas Reddy <tejasreddy353@gmail.com>
2026-03-15 13:06:51 +0530

    Seek to Implementation

--- src/components/PlayBackBar.tsx
+++ src/components/PlayBackBar.tsx
@@ -10,29 +10,36 @@ type PlayBackBarProps = {
 export default function PlaybackBar({
   currentTime,
   duration,
-  onSeek
+  onSeek,
 }: PlayBackBarProps) {
-
-    const [width, setWidth] = useState(0)
+  const [width, setWidth] = useState(0);
 
   const progress = currentTime / duration;
 
   const formatTime = (time: number) => {
     const mins = Math.floor(time / 60);
     const secs = Math.floor(time % 60);
-    return `${mins}:${secs.toString().padStart(2,'0')}`
+    return `${mins}:${secs.toString().padStart(2, "0")}`;
   };
 
   const onHandleSeek = (event: GestureResponderEvent) => {
-    const pressX = event.nativeEvent.locationX
-    const precentage = pressX / width
-    const seekToSeconds = Math.min(Math.max(duration * precentage, 0), duration)
-    onSeek(seekToSeconds)
-  }
+    const pressX = event.nativeEvent.locationX;
+    const precentage = pressX / width;
+    const seekToSeconds = Math.min(
+      Math.max(duration * precentage, 0),
+      duration,
+    );
+    onSeek(seekToSeconds);
+  };
 
   return (
     <View className="gap-4">
-      <Pressable onLayout={(event) => setWidth(event.nativeEvent.layout.width)} onPress={onHandleSeek} className="w-full bg-gray-800 h-2 rounded-full justify-center">
+      <Pressable
+        onLayout={(event) => setWidth(event.nativeEvent.layout.width)}
+        onPress={onHandleSeek}
+        className="w-full bg-gray-800 h-2 rounded-full justify-center"
+        hitSlop={20}
+      >
         <View
           className="bg-orange-400 h-full rounded-full"
           style={{ width: `${progress * 100}%` }}
